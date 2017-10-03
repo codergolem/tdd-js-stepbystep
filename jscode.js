75a2ed71933ae9f3e93fd051802bc1758a5c8f72
Mario Castellanos <mariocast@posteo.de>
2017-10-03 20:12:06 +0200

    Validate happy path vut no  error cases. First test green

--- jscode.js
+++ jscode.js
@@ -3,7 +3,25 @@
     
     // Implementation
     function validateForm(form) {
+        // get the inputs
+        let result = {
+            errors: []
+        };
         
+        let isValid = true;
+        const inputs = form.querySelectorAll('input');
+        
+        for (let input of inputs) {
+            if (input.dataset.validation === 'alphabetical') {
+                isValid = isValid && /^[a-z]+$/i.test(input.value);   
+            }
+            if (input.dataset.validation === 'numeric') {
+                isValid = isValid && /^[0-9]+$/i.test(input.value);
+            }
+        }
+        
+        result.isValid = isValid;
+        return result;
     }
     
     // Test Setup
@@ -18,7 +36,21 @@
         });
         
         describe('the validateForm function', function () {
-            // Write your first test case here :)
+            it("should evaluate valid inputs as valid", function() {
+                const name = form.querySelector('input[name="first-name"]');
+                const age = form.querySelector('input[name="age"]');
+                
+                name.value = "Alice";
+                age.value = "25";
+                
+                const result = validateForm(form);
+                const isFormValid = result.isValid;
+                const errors = result.errors;
+                
+                expect (isFormValid).to.be.true;
+                expect (errors.length).to.equal(0);
+                
+            });
         });
     });
     
